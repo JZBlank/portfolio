a3fc8d292691747d7c6a9363e95c0a6c0f45b8f2
JoyceZhang <joycezhang2065@gmail.com>
2022-08-24 18:08:12 -0400

    added Components

--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -26,7 +26,6 @@ export default function WelcomePage(){
                     </Typography>
                 </Stack>
             </Grid>
-            TEST
             
         </Grid>
     </div>
