8e75c297db5baacfdee96133a25c3b1a0dbf1e5c
joycezhang2065 <joycezhang2065@gmail.com>
2022-11-15 20:56:34 -0500

    added skills section layout

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,22 +1,29 @@
+import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid'; // Grid version 1
 import { Container } from '@mui/system';
 
 const skillInfo = 
     <div>
-        <Container>
-            
+        <Container sx={{height: '100px', bgcolor: 'red'}}>
+            <Typography sx={{top: '100px'}}>Test</Typography>
         </Container>
 
     </div>;
 
 export default function Skills(){
     return <div>
-        <Grid xs={1} md={1}>
-            {skillInfo}
-            {skillInfo}
-            {skillInfo}
+        <Grid container spacing= {2} position= 'relative' sx={{top:'200px'}}>
+            <Grid xs={1} md={4}>
+                {skillInfo}
+            </Grid>
 
+            <Grid xs={1} md={4}>
+                {skillInfo}
+            </Grid>
 
+            <Grid xs={1} md={4}>
+                {skillInfo}
+            </Grid>
 
         </Grid>
     </div>
