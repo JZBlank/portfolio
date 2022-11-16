612851170f224f753c3eff645c92ef6663c3569f
joycezhang2065 <joycezhang2065@gmail.com>
2022-11-16 01:16:26 -0500

    added skill topic visuals

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -2,29 +2,38 @@ import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid'; // Grid version 1
 import { Container } from '@mui/system';
 
-const skillInfo = 
-    <div>
-        <Container sx={{height: '100px', bgcolor: 'red'}}>
-            <Container sx={{margin: 'auto', padding: '10px', top: '60px', height:'20px', width: '40px', bgcolor: 'blue' }}>
-                <Typography sx={{top: '100px', textAlign: 'center'}}>Test</Typography>
-            </Container>
-        </Container>
-
-    </div>;
-
 export default function Skills(){
     return <div>
-        <Grid container spacing= {2} position= 'relative' sx={{top:'200px'}}>
+        <Grid container spacing= {2} position= 'relative' sx={{top:'100px'}}>
             <Grid xs={12} md={4}>
-                {skillInfo}
+                <Container sx={{height: '500px'}}>
+                    <Container sx={{top: '100px', margin: 'auto', padding: '10px', top: '60px', height:'35%', width: '70%', border: '7px solid #797EF6' }}>
+                        <Typography sx={{ fontSize: '2vw', marginTop: '10%', textAlign: 'center'}}>Programming Languages</Typography>
+                    </Container>
+
+                    <Container sx={{top: '10px', height: '0.1px', width: '35%', border: '3px solid #797EF6' }}></Container>
+
+                </Container>
             </Grid>
 
             <Grid xs={12} md={4}>
-                {skillInfo}
+                <Container sx={{height: '500px'}}>
+                    <Container sx={{top: '100px', margin: 'auto', padding: '10px', top: '60px', height:'35%', width: '70%', border: '7px solid #797EF6' }}>
+                        <Typography sx={{ fontSize: '2vw', marginTop: '10%', textAlign: 'center'}}>Technologies and Tools</Typography>
+                    </Container>
+
+                    <Container sx={{top: '10px', height: '0.1px', width: '35%', border: '3px solid #797EF6' }}></Container>
+                </Container>
             </Grid>
 
             <Grid xs={12} md={4}>
-                {skillInfo}
+                <Container sx={{height: '500px'}}>
+                    <Container sx={{top: '100px', margin: 'auto', padding: '10px', top: '60px', height:'35%', width: '70%', border: '7px solid #797EF6' }}>
+                        <Typography sx={{ fontSize: '2vw', marginTop: '10%', textAlign: 'center'}}>Web Development</Typography>
+                    </Container>
+
+                    <Container sx={{top: '10px', height: '0.1px', width: '35%', border: '3px solid #797EF6' }}></Container>
+                </Container>
             </Grid>
 
         </Grid>
